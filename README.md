17220ad5a8f6f0f76d800c20e873e89d188ee816
WhosRed <88060235+WhosRed@users.noreply.github.com>
2023-01-02 13:25:11 -0600

    I forgor

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# RustyMail
+# **Sendr** (WIP)
 
-RustyMail is an email CLI based on the [lettre](https://github.com/lettre/lettre) library written in Rust.
+Sendr is a basic program that forwards emails received by one account (presumably a sentinel email address) to another (a primary address) using the lettre crate.
+
+When ran, sendr will look through the contents of the sentinel inbox and forward all unread mail to the given primary address and then mark them as read.
+
+**This project is currently a work in progress and is non functioning as of the latest commit.**
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use lettre::Message;
-use lettre::Transport;
-use lettre::SmtpTransport;
 use clap::Parser;
-use clap::arg;
+use lettre::Message;
+use lettre::SmtpTransport;
+use lettre::Transport;
+use lettre::message::Mailbox;
 
 #[derive(Parser)]
 struct Cli {
@@ -19,16 +19,53 @@ struct Cli {
     body: String
 }
 
+/// Sends an email, returns false if operation fails and prints an error message else returns true.
+fn send_email(f: String, t: String, s: String, b: String) -> bool {
+    let from_result = f.parse();
+    let from = match from_result {
+        Ok(f_add) => f_add,
+        Err(f_err) => {
+            println!("Error Sending Email - Invalid Sender Address:  {}", f_err);
+            return false;
+        }
+    };
+    
+    let to_result = t.parse();
+    let to = match to_result {
+        Ok(t_add) => t_add,
+        Err(t_err) => {
+            println!("Error Sending Email - Invalid Receiver Address:  {}", t_err);
+            return false;
+        }
+    };
+
+    let email_result = Message::builder()
+                        .from(Mailbox::new(None, from))
+                        .to(Mailbox::new(None, to))
+                        .subject(s)
+                        .body(b);
+
+    let email = match email_result {
+        Ok(em) => em,
+        Err(ee) => {
+            println!("Error Sending Email - Email Build Error:  {}", ee);
+            return false;
+        }
+    };
+
+    let sender = SmtpTransport::unencrypted_localhost();
+    let res = sender.send(&email);
+
+    if res.is_err() {
+        println!("Unable To Send Email:  {}", res.unwrap_err());
+        return false;
+    }
+
+    println!("Email Sent Successfully!");
+    return true;
+}
+
 fn main() {
     let args = Cli::parse();
-
-    let email = Message::builder()
-                                .from(args.from.parse().unwrap())
-                                .to(args.to.parse().unwrap())
-                                .subject(args.subject)
-                                .body(args.body)
-                                .unwrap();
-
-    let mailer = SmtpTransport::unencrypted_localhost();
-    mailer.send(&email).unwrap();
+    send_email(args.from, args.to, args.subject, args.body);
 }
